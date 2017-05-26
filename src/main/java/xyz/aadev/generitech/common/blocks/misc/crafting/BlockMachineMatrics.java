5d6489d5f8d26bade0007f74bc145a5eaf63c772
mailboxkiller <kovalchickzz@gmail.com>
2017-05-26 15:19:51 -0500

    quick push

--- src/main/java/xyz/aadev/generitech/common/blocks/misc/crafting/BlockMachineMatrics.java
+++ src/main/java/xyz/aadev/generitech/common/blocks/misc/crafting/BlockMachineMatrics.java
@@ -57,7 +57,7 @@ public class BlockMachineMatrics extends BlockTierBase {
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
 
 
-        return true;
+        return false;
     }
 
     @Override
